fdc8664873bc58d88747d356a1c2abb1ecfcb19d
Daniel Wiesmann <bakongo@gmail.com>
2007-12-13 12:10:22 +0000

    add make_multiple_line_chart

--- lib/chartr.rb
+++ lib/chartr.rb
@@ -1,9 +1,11 @@
 class Chartr
 
+  COLORS = ['FF0000', '00FF00', '0000FF', '000000']
+  
   def self.simple_encode(values)
     simple_values = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     max_value = values.max
-    chart_data = ['s:'];
+    chart_data = [""]
     values.each do |v|
       val = Float(v)
       if (!val.nan? && v >= 0)   
@@ -19,10 +21,33 @@ class Chartr
   def self.make_simple_line_chart(values, labels, title, size="200x100")
     
     image_str =<<-START
-    <img src="http://chart.apis.google.com/chart?chs=#{size}&amp;chd=#{simple_encode(values)}&amp;cht=lc&amp;chxt=x,y&amp;chxl=0:|#{labels.join('|')}|1:||#{values.max}" alt="#{title}" />
+    <img src="http://chart.apis.google.com/chart?chs=#{size}&amp;chd=s:#{simple_encode(values)}&amp;cht=lc&amp;chxt=x,y&amp;chxl=0:|#{labels.join('|')}|1:||#{values.max}" alt="#{title}" />
     START
 
     image_str
   end
+  
+  
+  def self.make_multiple_line_chart(value_sets, labels, title, size="200x100")
+    
+    data_sets = []
+    set_colors = []
+    max = 0
+    value_sets.each_with_index do |value_set, index|
+      data_sets << simple_encode(value_set)
+      max = value_set.max
+      set_colors << COLORS[index % COLORS.length]
+    end
+    
+    data = "&amp;chd=s:#{data_sets.join(',')}"
+    colors ="&amp;chco=#{set_colors.join(',')}"
+    image_str =<<-START
+    <img src="http://chart.apis.google.com/chart?chs=#{size}&amp;#{data}#{colors}&amp;cht=lc&amp;chxt=x,y&amp;chxl=0:|#{labels.join('|')}|1:||#{max}" alt="#{title}" />
+    START
+
+    image_str
+  end
+
+  
   
 end
