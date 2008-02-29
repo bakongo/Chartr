e4eb4036c54e23d7b53c846ed552c07f6644e594
Daniel Wiesmann <bakongo@gmail.com>
2008-02-29 15:28:58 +0000

    add bar charts

--- lib/chartr.rb
+++ lib/chartr.rb
@@ -46,8 +46,43 @@ class Chartr
     data = "&amp;chd=s:#{data_sets.join(',')}"
     colors ="&amp;chco=#{set_colors.join(',')}"
     legend = "&amp;chdl=#{legend_labels.join('|')}"
+    x_axis = "&amp;chxt=x,y&amp;chxl=0:|#{labels.join('|')}|1:|0|#{max}"
     image_str =<<-START
-    <img src="http://chart.apis.google.com/chart?chs=#{size}&amp;#{data}#{legend}#{colors}&amp;cht=lc&amp;chxt=x,y&amp;chxl=0:|#{labels.join('|')}|1:||#{max}" alt="#{title}" />
+    <img src="http://chart.apis.google.com/chart?chs=#{size}&amp;#{data}#{legend}#{colors}&amp;cht=lc&amp;" alt="#{title}" />
+    START
+
+    image_str
+  end
+  
+  
+  # make a stacked bar chart
+  #
+  #
+  # the colors array is used if provided. the color used will cycle through the values in this array.
+  def self.make_stacked_bar_chart(value_sets, labels, legend_labels, title, size="200x100", max_y = nil, colors = nil)
+    
+    data_sets = []
+    set_colors = []
+    max = 0
+    # find the max value, which we provide to simple encode 
+    # so that all data is graphed with the same max. graphs don't
+    # make sense otherwise
+    max = max_y || value_sets.flatten.max
+    
+    value_sets.each_with_index do |value_set, index|
+      data_sets << simple_encode(value_set, max)
+      if colors
+        set_colors << colors[index % colors.length]   
+      else
+        set_colors << COLORS[index % COLORS.length]
+      end
+    end
+
+    data = "&amp;chd=s:#{data_sets.join(',')}"
+    colors ="&amp;chco=#{set_colors.join(',')}"
+    legend = "&amp;chdl=#{legend_labels.join('|')}"
+    image_str =<<-START
+    <img src="http://chart.apis.google.com/chart?chs=#{size}&amp;#{data}#{legend}#{colors}&amp;cht=bvs&amp;chxt=x,y&amp;chxl=0:|#{labels.join('|')}|1:||#{max}" alt="#{title}" />
     START
 
     image_str
